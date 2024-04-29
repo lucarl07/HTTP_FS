6284cc16085c8587af36aaa52228f6efa0248017
lucarl07 <junitinho2007@gmail.com>
2024-04-29 10:07:59 -0300

    Alterar a posição dos códigos

--- server.js
+++ server.js
@@ -23,19 +23,19 @@ const server = http.createServer((req, res) => {
     }
 
     // ENDPOINTS:
-    if (url === "/empregados" && method === "GET") { 
-      // Lista todos os funcionários.
+    if (url === "/empregados" && method === "GET") { // Lista todos os funcionários.
+      
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify(jsonData));
 
-    } else if (url === "/empregados/count" && method === "GET") {
-      // Obtêm o total de funcionários cadastrados.
+    } else if (url === "/empregados/count" && method === "GET") { // Obtêm o total de funcionários cadastrados.
+      
       const nOfEmployees = jsonData.length;
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify({ "Número de funcionários": nOfEmployees }));
 
-    } else if (url.startsWith("/empregados/porCargo/") && method === "GET") {
-      // Listar os funcionários por cargo detido.
+    } else if (url.startsWith("/empregados/porCargo/") && method === "GET") { // Listar os funcionários por cargo detido.
+      
       const office = url.split("/")[3].toLowerCase();
       const employees = jsonData.filter(emp => emp.cargo.toLowerCase() === office);
       
@@ -47,8 +47,8 @@ const server = http.createServer((req, res) => {
         res.end(JSON.stringify({ message: "Cargo inexistente na base de dados." }));
       }
 
-    } else if (url.startsWith("/empregados/porHabilidade/") && method === "GET") {
-      // Listar os funcionários por habilidade detida.
+    } else if (url.startsWith("/empregados/porHabilidade/") && method === "GET") { // Listar os funcionários por habilidade detida.
+      
       const skill = url.split("/")[3].toLowerCase();
       console.log(skill);
 
@@ -67,13 +67,19 @@ const server = http.createServer((req, res) => {
         res.end(JSON.stringify({ message: "Habilidade inexistente na base de dados." }));
       }
 
-    } else if (url.startsWith("/empregados/porFaixaSalarial?") && method === "GET") {
-      // Listar funcionários por faixa salarial (A FINALIZAR)
+    } else if (url.startsWith("/empregados/porFaixaSalarial?") && method === "GET") { // Listar funcionários por faixa salarial (A FINALIZAR)
+      
       const attributes = url.split("?")[1]
-      console.log(attributes)
+      console.log(attributes);
+      
+      res.writeHead(204, { "Content-Type": "application/json" })
+      res.end(JSON.stringify({ 
+        output: "Não há nada a ser pesquisado no momento.",
+        message: "O endpoint ainda está sendo construído."
+      }));
 
-    } else if (url === "/empregados" && method === "POST") {
-      // Adiciona um funcionário novo.
+    } else if (url === "/empregados" && method === "POST") { // Adiciona um funcionário novo.
+      
       let body = "";
 
       req.on("data", (chunk) => {
@@ -130,8 +136,8 @@ const server = http.createServer((req, res) => {
         );
       });
       
-    } else if (url.startsWith("/empregados/") && method === "PUT") {
-      // Procura um funcionário pelo ID e atualizar suas informações.
+    } else if (url.startsWith("/empregados/") && method === "PUT") { // Procura um funcionário pelo ID e atualizar suas informações.
+      
       const id = parseInt(url.split("/")[2]); 
       let body = "";
 
@@ -177,8 +183,8 @@ const server = http.createServer((req, res) => {
           res.end(JSON.stringify({ message: "Funcionário não encontrado ou senha não inserida." }));
         }
       });
-    } else if (url.startsWith("/empregados/") && method === "DELETE") {
-      // Remove um funcionário pelo ID.
+    } else if (url.startsWith("/empregados/") && method === "DELETE") { // Remove um funcionário pelo ID.
+      
       const id = parseInt(url.split("/")[2]); 
       const index = jsonData.findIndex((item) => item.id === id);
 
@@ -204,8 +210,8 @@ const server = http.createServer((req, res) => {
         res.writeHead(404, { "Content-Type": "application/json" });
         res.end(JSON.stringify({ message: "Livro não encontrado" }));
       }
-    } else if (url.startsWith("/empregados/") && method === "GET") {
-      // Lista um funcionário com base em seu ID.
+    } else if (url.startsWith("/empregados/") && method === "GET") { // Lista um funcionário com base em seu ID.
+      
       const id = parseInt(url.split("/")[2]);
       const index = jsonData.findIndex((item) => item.id === id);
 
